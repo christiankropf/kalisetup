471d439d5a17cb309fa9b3e855caa4a52f34dda7
christiankropf <47499923+christiankropf@users.noreply.github.com>
2019-02-10 20:40:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,31 +3,56 @@ message="Test setup script"
 update="N"
 
 echo "$message"
-#!/bin/bash
-#Abfrage ob Updates durchgeführt werden sollen
-
-read -p "Sollen Updates durchgeführt werden (Y/N):" update
-If update="Y"
-    echo Updates werden durchgeführt: 
-
-    #Inital updates & installations
-    apt-get update
-    apt-get install terminator
-    apt-get update && apt-get upgrade -y
-    apt-get dist-upgrade -y
-    apt-get install virtualbox-guest-x11 –y
-    nmap --script-updatedb
-Else
-
-#Setup and configuration
-echo "Setup und configuration"
-
-＃ Tastaturlayout einstellen:
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us')]"
-＃ Zeitzone einstellen:
-timedatectl set-timezone Europe/Zurich
-＃ Zeit einstellen mit ntpdate:
-apt-get install ntpdate -y
+#!/bin/bash 
+#Abfrage ob Updates durchgeführt werden sollen 
+ 
+echo "Sollen Updates durchgeführt werden (Y/N):" 
+read update 
+echo $update 
+if [ $update == "Y" ] 
+        then 
+                echo Updates werden durchgeführt:  
+ 
+                 #Inital updates & installations 
+                 apt-get update 
+                 apt-get install terminator 
+                 apt-get update && apt-get upgrade -y 
+                 apt-get dist-upgrade -y 
+                 apt-get install virtualbox-guest-x11 –y 
+                 nmap --script-updatedb 
+        else 
+ 
+                echo Keine Updates werden durchgeführt:  
+ 
+fi 
+ 
+#Setup and configuration 
+echo "Setup und configuration" 
+ 
+#Tastaturlayout einstellen: 
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'ch')]" 
+#Zeitzone einstellen: 
+timedatectl set-timezone Europe/Zurich 
+#Zeit einstellen mit ntpdate: 
+apt-get install ntpdate -y 
 ntpdate ch.pool.ntp.org
+
+
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+-- VISUAL --                                                                                                                                                                           33        6,10          All
+
 
 
