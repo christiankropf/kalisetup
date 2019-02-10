dfad3ff8ecc02d2f8f4987852212fa779e6a259e
christiankropf <47499923+christiankropf@users.noreply.github.com>
2019-02-10 17:00:20 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# Variables
+message="Test setup script"
+#Inital updates & installations
+
+#apt-get update
+#apt-get install terminator
+#apt-get update && apt-get upgrade -y
+#apt-get dist-upgrade -y
+#apt-get install virtualbox-guest-x11 –y
+#apt-get install terminator (Praktische Terminator Shell)
+#nmap --script-updatedb
+
+#Setup and configuration
+echo "$message"
